1b2336711bf6bd316d4c48ac88037c9ce3bc990f
nelkor <a.tyutyunnik@intterra.ru>
2021-07-19 17:15:22 +0300

    rethinking viewport state

--- src/modules/map/viewport/index.ts
+++ src/modules/map/viewport/index.ts
@@ -2,12 +2,14 @@ import { originCenter } from '@/world'
 
 type ComputedState = {
   top: number
-  bottom: number
   left: number
+  bottom: number
   right: number
   zoom: number
 }
 
+// TODO center & zoom – animatedState
+//  Начать с zoom (+ кнопки)
 const state = {
   width: 0,
   height: 0,
@@ -15,45 +17,36 @@ const state = {
   zoom: 1.4,
 }
 
-const computedState: ComputedState = {
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  zoom: 0,
-}
-
-const computeState = () => {
-  const { width, height, center, zoom } = state
-  const scaledWidth = width / zoom
-  const scaledHeight = height / zoom
-
-  computedState.zoom = zoom
-  computedState.top = center.y - scaledHeight / 2
-  computedState.left = center.x - scaledWidth / 2
-  computedState.bottom = computedState.top + scaledHeight
-  computedState.right = computedState.left + scaledWidth
-}
-
 export const setSize = (width: number, height: number): void => {
   state.width = width
   state.height = height
-
-  computeState()
 }
 
 export const setCenter = (x: number, y: number): void => {
   state.center.x = x
   state.center.y = y
-
-  computeState()
 }
 
 export const setZoom = (zoom: number): void => {
   state.zoom = Math.max(1, zoom)
-
-  computeState()
 }
 
 export const getCenter = (): Readonly<Point> => state.center
-export const getViewport = (): Readonly<ComputedState> => computedState
+
+// TODO функцию для остановки анимации центра?
+
+export const getViewport = (): Readonly<ComputedState> => {
+  const { width, height, center, zoom } = state
+  const scaledWidth = width / zoom
+  const scaledHeight = height / zoom
+  const top = center.y - scaledHeight / 2
+  const left = center.x - scaledWidth / 2
+
+  return {
+    top,
+    left,
+    bottom: top + scaledHeight,
+    right: left + scaledWidth,
+    zoom,
+  }
+}
